e33cabfee08a2ee3ae39db435c9529e0bcb3f4d5
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-09-06 14:30:19 +0300

    Updates

---
+++ precache-manifest.ed167b4255db92babd57d0376ab7f096.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b453996354a6181870a39f36ad88e055",
+    "url": "/ellu-laitinen/portfolio/index.html"
+  },
+  {
+    "revision": "94fb6a4f242d7269e0d4",
+    "url": "/ellu-laitinen/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b4d692581dc5c7ac0525",
+    "url": "/ellu-laitinen/portfolio/static/css/main.3f852da2.chunk.css"
+  },
+  {
+    "revision": "94fb6a4f242d7269e0d4",
+    "url": "/ellu-laitinen/portfolio/static/js/2.22b5c2a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ellu-laitinen/portfolio/static/js/2.22b5c2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d692581dc5c7ac0525",
+    "url": "/ellu-laitinen/portfolio/static/js/main.50a9ae75.chunk.js"
+  },
+  {
+    "revision": "825f366acd735f8a47fb",
+    "url": "/ellu-laitinen/portfolio/static/js/runtime-main.d49b7a9a.js"
+  }
+]);

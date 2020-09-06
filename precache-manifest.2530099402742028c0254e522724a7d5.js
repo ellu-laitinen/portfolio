2f24dbb342b2215d95db5aebb6d77ba2d67b9709
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-09-06 14:37:49 +0300

    Updates

---
+++ precache-manifest.2530099402742028c0254e522724a7d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d52b0b65df70c0a229f53a831886f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94fb6a4f242d7269e0d4",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b4d692581dc5c7ac0525",
+    "url": "/static/css/main.3f852da2.chunk.css"
+  },
+  {
+    "revision": "94fb6a4f242d7269e0d4",
+    "url": "/static/js/2.22b5c2a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.22b5c2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d692581dc5c7ac0525",
+    "url": "/static/js/main.50a9ae75.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
